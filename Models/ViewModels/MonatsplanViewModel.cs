2f81fbfbcd34114bf1db337e025937848d8b7e09
Bowseer <andre.palmen.1998@gmail.com>
2026-04-11 16:01:14 +0200

    fix

--- Models/ViewModels/MonatsplanViewModel.cs
+++ Models/ViewModels/MonatsplanViewModel.cs
@@ -1,21 +1,50 @@
-using Schichtplaner.Models;
-
 namespace Schichtplaner.Models.ViewModels;
 
 public class MonatsplanViewModel
 {
     public int Jahr { get; set; }
     public int Monat { get; set; }
-    public List<MitarbeiterMonatsplanRow> MitarbeiterRows { get; set; } = new();
+
+    public int? StandortId { get; set; }
+    public List<StandortDto> Standorte { get; set; } = new();
+
+    public List<MitarbeiterSidebarDto> Mitarbeiter { get; set; } = new();
+
+    public List<KalenderWocheDto> Wochen { get; set; } = new();
 }
 
-public class MitarbeiterMonatsplanRow
+public class StandortDto
 {
-    public int MitarbeiterId { get; set; }
-    public string MitarbeiterName { get; set; } = string.Empty;
-    public string StandortName { get; set; } = string.Empty;
-    public decimal MaxStunden { get; set; }
-    public decimal GeplanteStunden { get; set; }
-    public bool Ueberschritten { get; set; }
-    public List<Schicht> Schichten { get; set; } = new();
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
+
+public class MitarbeiterSidebarDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Farbe { get; set; } = string.Empty;
+    public decimal Reststunden { get; set; }
+}
+
+public class KalenderWocheDto
+{
+    public List<KalenderTagDto> Tage { get; set; } = new();
+}
+
+public class KalenderTagDto
+{
+    public DateOnly Datum { get; set; }
+    public bool IstSonntag { get; set; }
+    public bool IstFeiertag { get; set; }
+    public string? FeiertagName { get; set; }
+
+    public List<KalenderSlotDto> Slots { get; set; } = new();
+}
+
+public class KalenderSlotDto
+{
+    public int Slot { get; set; }
+    public string? MitarbeiterName { get; set; }
+    public string? Farbe { get; set; }
 }
