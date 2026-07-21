14f09fa1128836111a4796e760e74f421ea0872a
Bowseer <andre.palmen.1998@gmail.com>
2026-07-21 18:07:04 +0200

    Fix German umlauts across the UI

--- Models/Bundesland.cs
+++ Models/Bundesland.cs
@@ -2,6 +2,7 @@
 
 public enum Bundesland
 {
+    [System.ComponentModel.DataAnnotations.Display(Name = "Baden-Württemberg")]
     BadenWuerttemberg = 1,
     Bayern = 2,
     Berlin = 3,
@@ -9,13 +10,19 @@ public enum Bundesland
     Bremen = 5,
     Hamburg = 6,
     Hessen = 7,
+    [System.ComponentModel.DataAnnotations.Display(Name = "Mecklenburg-Vorpommern")]
     MecklenburgVorpommern = 8,
     Niedersachsen = 9,
+    [System.ComponentModel.DataAnnotations.Display(Name = "Nordrhein-Westfalen")]
     NordrheinWestfalen = 10,
+    [System.ComponentModel.DataAnnotations.Display(Name = "Rheinland-Pfalz")]
     RheinlandPfalz = 11,
     Saarland = 12,
     Sachsen = 13,
+    [System.ComponentModel.DataAnnotations.Display(Name = "Sachsen-Anhalt")]
     SachsenAnhalt = 14,
+    [System.ComponentModel.DataAnnotations.Display(Name = "Schleswig-Holstein")]
     SchleswigHolstein = 15,
+    [System.ComponentModel.DataAnnotations.Display(Name = "Thüringen")]
     Thueringen = 16
 }
